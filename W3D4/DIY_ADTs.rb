f02da482e81ed0865e6dfb2511414d08b41022cb
Yizhe Wang <yizhewang@Yizhes-MacBook-Pro.local>
2020-02-27 22:54:05 -0800

    week 3 day 4 homework

---
+++ W3D4/DIY_ADTs.rb
@@ -0,0 +1,72 @@
+class Stack
+    def initialize(stack = [])
+        @stack = stack
+    end
+
+    def push(el)
+        @stack.push(el)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+end
+
+class Queue
+    def initialize(queue = [])
+        @queue = queue
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue[0]
+    end
+end
+
+class Map
+    def initialize(map = [])
+        @map = map
+    end
+
+    def set(key, value)
+        flag = false
+        @map.each do |sub|
+            if sub[0] == key
+                sub[1] = value
+                flag = true
+                return @map
+            end 
+        end
+        if flag == false
+            @map << [key, value]
+        end
+    end
+
+    def get(key)
+        @map.each do |sub|
+            if sub[0] == key
+                return sub[1]
+            end
+        end
+        return nil
+    end
+
+    def delete(key)
+        @map.select {|sub| sub[0] != key}
+    end
+end
+
+stack = Stack.new([1,2,3,4,5])
+queue = Queue.new([5,4,3,2,1])
+map = Map.new()
